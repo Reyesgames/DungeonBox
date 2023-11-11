49ee321a4aa59eb27b10455aeb5a7beada07004b
Reyesgames <128988837+Reyesgames@users.noreply.github.com>
2023-11-11 13:49:35 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1699523356,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/spr_bottomcol-sheet0.png",
+		"images/spr_col-sheet0.png",
+		"images/spr_ball-sheet0.png",
+		"images/spr_bg-sheet0.png",
+		"images/spr_btn_play-sheet0.png",
+		"images/spr_audio-sheet0.png",
+		"images/spr_audio-sheet1.png",
+		"media/bigshoot1.m4a",
+		"media/bigshoot1.ogg",
+		"media/glass.m4a",
+		"media/glass.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
